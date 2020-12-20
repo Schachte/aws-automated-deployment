94fee4c98dea17444f6d802bd1ed9280c3a975ea
Ryan Schachte <code@ryan-schachte.com>
2020-12-20 15:24:51 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Testintg 2 
         </a>
       </header>
     </div>
